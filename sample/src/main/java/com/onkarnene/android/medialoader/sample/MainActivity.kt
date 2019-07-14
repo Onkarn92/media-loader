775d2a9197b8354e699f09edb40605ee7c4443a3
Onkar Nene <omkarn.92@gmail.com>
2019-07-14 12:43:50 +0530

    #6 Sample app tested with memory cache.

--- sample/src/main/java/com/onkarnene/android/medialoader/sample/MainActivity.kt
+++ sample/src/main/java/com/onkarnene/android/medialoader/sample/MainActivity.kt
@@ -7,6 +7,7 @@
 package com.onkarnene.android.medialoader.sample
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import com.onkarnene.android.medialoader.MediaLoader
@@ -15,6 +16,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 	private val full =
 			"https://images.unsplash.com/photo-1464547323744-4edd0cd0c746?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=17b4934ff6fe2e8773896c87aa4ae85b"
+	
+	private val regular =
+			"https://images.unsplash.com/photo-1464547323744-4edd0cd0c746?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=c990dc1cd803124b9e6c43b97c844ad3"
+	
 	private val url1 =
 			"https://images.unsplash.com/photo-1464550883968-cec281c19761?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=200&fit=max&s=9fba74be19d78b1aa2495c0200b9fbce"
 	
@@ -34,9 +39,12 @@ class MainActivity : AppCompatActivity() {
 	
 	override fun onResume() {
 		super.onResume()
-		MediaLoader.Builder<AppCompatImageView>(this).load(full).into(image).create().download()
-		MediaLoader.Builder<AppCompatImageView>(this).load(url2).into(image1).create().download()
-		MediaLoader.Builder<AppCompatImageView>(this).load(url3).into(image2).create().download()
-		MediaLoader.Builder<AppCompatImageView>(this).load(url4).into(image3).create().download()
+		MediaLoader.Builder<AppCompatImageView>(this).load(regular).into(image).create().download()
+		val handler = Handler()
+		handler.postDelayed({
+			                    MediaLoader.Builder<AppCompatImageView>(this).load(regular).into(image1).create().download()
+			                    MediaLoader.Builder<AppCompatImageView>(this).load(regular).into(image2).create().download()
+			                    MediaLoader.Builder<AppCompatImageView>(this).load(regular).into(image3).create().download()
+		                    }, 5000)
 	}
 }
